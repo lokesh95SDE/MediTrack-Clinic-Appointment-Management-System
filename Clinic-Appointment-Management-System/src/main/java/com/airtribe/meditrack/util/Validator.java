e1c0ecce002781836f8cea6a984a95643623bb9c
Lokesh <lokesh95info@gmail.com>
2026-04-20 01:22:40 +0530

    MediTrack updated Validator

---
+++ Clinic-Appointment-Management-System/src/main/java/com/airtribe/meditrack/util/Validator.java
@@ -0,0 +1,23 @@
+package com.airtribe.meditrack.util;
+import com.airtribe.meditrack.exception.InvalidDataException;
+
+public class Validator {
+
+    public static void validateName(String name) throws InvalidDataException {
+        if (name == null || name.trim().isEmpty()) {
+            throw new InvalidDataException("Name cannot be empty");
+        }
+    }
+
+    public static void validateAge(int age) throws InvalidDataException {
+        if (age <= 0 || age > 120) {
+            throw new InvalidDataException("Invalid age");
+        }
+    }
+
+    public static void validateFee(double fee) throws InvalidDataException {
+        if (fee < 0) {
+            throw new InvalidDataException("Fee cannot be negative");
+        }
+    }
+}
